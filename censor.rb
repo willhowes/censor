f866e620984249f07db1953d980d4686e003e135
eldenhowes <williameldenhowes@gmail.com>
2019-06-03 15:33:01 +0100

    First commit

---
+++ censor.rb
@@ -0,0 +1,19 @@
+def censor(array)
+  banned_phrases = ["sucks", "bad", "hate", "foolish", "danger to society"]
+  array.each do |phrase|
+    banned_phrases.each do |x|
+      if phrase.include?(x)
+        puts "This phrase should be censored: #{phrase}"
+      end
+    end
+  end
+end
+
+test_tweets = [
+  "This president sucks!",
+  "I hate this Blank House!",
+  "I can't believe we're living with such a bad leadership. We were so foolish",
+  "President Presidentname is a danger to society. I hate that he's so bad – it sucks."
+  ]
+
+censor(test_tweets)
